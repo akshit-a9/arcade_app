2935ef0fafddd7fcdc97d1702aaa6b0876e8ed86
akshit-a9 <akshitsharma2002@gmail.com>
2024-07-06 09:28:07 +0530

    Kindle UI

--- lib/main.dart
+++ lib/main.dart
@@ -32,34 +32,85 @@ class ArcadeHome extends StatelessWidget {
           ),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             SizedBox(height: 50),
-            Text(
-              'Arcade',
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+            Center(
+              child: Text(
+                'Arcade',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
             ),
-            SizedBox(height: 30),
+            SizedBox(height: 20),
             Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                padding: EdgeInsets.all(20),
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20,
-                children: <Widget>[
-                  buildGameButton(context, 'Spin&Win', 'assets/images/logo_1.png', spinwheel.MyApp()),
-                  buildGameButton(context, 'GoldenSlots', 'assets/images/logo_2.png', slot_game.MyNewApp()),
-                  buildGameButton(context, 'Match Master', 'assets/images/logo_3.png', memory_match.MyApp()),
-                  buildGameButton(context, 'Drive Through', 'assets/images/logo_placeholder.png', null), // Placeholder for future game
-                ],
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    buildSectionTitle('Featured Games'),
+                    buildGameCarousel(context, [
+                      buildGameButton(context, 'Spin&Win', 'assets/images/logo_1.png', spinwheel.MyApp()),
+                      buildGameButton(context, 'GoldenSlots', 'assets/images/logo_2.png', slot_game.MyNewApp()),
+                      buildGameButton(context, 'Match Master', 'assets/images/logo_3.png', memory_match.MyApp()),
+                      buildGameButton(context, 'Drive Through', 'assets/images/logo_placeholder.png', null),
+                    ]),
+                    buildSectionTitle('All Games'),
+                    buildGameGrid(context, [
+                      buildGameButton(context, 'Spin&Win', 'assets/images/logo_1.png', spinwheel.MyApp()),
+                      buildGameButton(context, 'GoldenSlots', 'assets/images/logo_2.png', slot_game.MyNewApp()),
+                      buildGameButton(context, 'Match Master', 'assets/images/logo_3.png', memory_match.MyApp()),
+                      buildGameButton(context, 'Drive Through', 'assets/images/logo_placeholder.png', null),
+                    ]),
+                  ],
+                ),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget buildSectionTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  Widget buildGameCarousel(BuildContext context, List<Widget> gameButtons) {
+    return Container(
+      height: 200,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: gameButtons.map((gameButton) => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: gameButton,
+        )).toList(),
+      ),
+    );
+  }
+
+  Widget buildGameGrid(BuildContext context, List<Widget> gameButtons) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridView.count(
+        shrinkWrap: true,
+        crossAxisCount: 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+        children: gameButtons,
       ),
     );
   }
@@ -74,25 +125,26 @@ class ArcadeHome extends StatelessWidget {
         );
       }
           : null,
-      child: Card(
-        color: Colors.transparent,
-        elevation: 5,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imagePath, width: 80, height: 80),
-            SizedBox(height: 10),
-            Text(
-              title,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-              textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
             ),
-          ],
-        ),
+            child: Image.asset(imagePath, width: 100, height: 100),
+          ),
+          SizedBox(height: 10),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
