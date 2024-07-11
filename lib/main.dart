12f00d7d5ff543beabc3f5d447b98981891bf868
akshit-a9 <akshitsharma2002@gmail.com>
2024-07-11 10:05:04 +0530

    Secondary splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -18,59 +18,59 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ArcadeHome(),
+      home: VideoSplashScreen(),
     );
   }
 }
 
-// class VideoSplashScreen extends StatefulWidget {
-//   @override
-//   _VideoSplashScreenState createState() => _VideoSplashScreenState();
-// }
-//
-// class _VideoSplashScreenState extends State<VideoSplashScreen> {
-//   VideoPlayerController? _controller;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _controller = VideoPlayerController.asset('assets/splash_video.mp4')
-//       ..initialize().then((_) {
-//         setState(() {});
-//         _controller!.play();
-//         _controller!.setLooping(false);
-//         _controller!.addListener(checkVideo);
-//       });
-//   }
-//
-//   void checkVideo() {
-//     if (_controller!.value.position == _controller!.value.duration) {
-//       Navigator.of(context).pushReplacement(MaterialPageRoute(
-//         builder: (context) => ArcadeHome(), // Your main screen
-//       ));
-//     }
-//   }
-//
-//   @override
-//   void dispose() {
-//     _controller!.dispose();
-//     super.dispose();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: _controller!.value.isInitialized
-//             ? AspectRatio(
-//           aspectRatio: _controller!.value.aspectRatio,
-//           child: VideoPlayer(_controller!),
-//         )
-//             : Container(),
-//       ),
-//     );
-//   }
-// }
+class VideoSplashScreen extends StatefulWidget {
+  @override
+  _VideoSplashScreenState createState() => _VideoSplashScreenState();
+}
+
+class _VideoSplashScreenState extends State<VideoSplashScreen> {
+  VideoPlayerController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/splash_video.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+        _controller!.play();
+        _controller!.setLooping(false);
+        _controller!.addListener(checkVideo);
+      });
+  }
+
+  void checkVideo() {
+    if (_controller!.value.position == _controller!.value.duration) {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => ArcadeHome(), // Your main screen
+      ));
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _controller!.value.isInitialized
+            ? AspectRatio(
+          aspectRatio: _controller!.value.aspectRatio,
+          child: VideoPlayer(_controller!),
+        )
+            : Container(),
+      ),
+    );
+  }
+}
 
 class ArcadeHome extends StatefulWidget {
   @override
